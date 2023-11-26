ec72921e9d8d36a4b1f760c8bba2f03a6502e75b
EduardoLoppes <mcfgonzalles@gmail.com>
2023-11-26 11:26:03 -0300

    Subindo o projeto

---
+++ CrudSimples/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudSimples.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+
+        public int Id { get; set; }
+
+        [Column("Descricao")]
+        [Display(Name = "Descricao")]
+
+        public string Name { get; set; }
+    }
+}
